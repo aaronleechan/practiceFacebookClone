1d182479dc2ec624aa8fb884949196b65c8c4752
aaronleechan <aaron171091@gmail.com>
2020-08-10 08:57:39 -0700

    update

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:facebookclone/config/palette.dart';
+import 'package:facebookclone/screens/screens.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Facebook Clone UI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        scaffoldBackgroundColor: Palette.scaffold,
+      ),
+      home: NavScreen(),
+    );
+  }
+}
